ae2f360c30eee9be9fa6f49c5e76532684f975a3
phoboz <phoboz@vmx.nu>
2018-12-26 08:50:44 +0100

    Optimized web background, saved a few cycles

--- source/web.c
+++ source/web.c
@@ -7,39 +7,45 @@
 // ---------------------------------------------------------------------------
 
 const signed char web[]=
-{	(signed char) +1, +10, -4, // sync and move to y, x
-	(signed char) -1, +0, +8, // draw, y, x
-	(signed char) -1, -6, +11, // draw, y, x
-	(signed char) -1, -8, +0, // draw, y, x
-	(signed char) -1, -6, -11, // draw, y, x
-	(signed char) -1, +0, -8, // draw, y, x
-	(signed char) -1, +6, -11, // draw, y, x
-	(signed char) -1, +8, +0, // draw, y, x
-	(signed char) -1, +6, +11, // draw, y, x
-	(signed char) -1, +102, -63, // draw, y, x
-	(signed char) +1, +10, +4, // sync and move to y, x
-	(signed char) -1, +99, +60, // draw, y, x
-	(signed char) +1, +4, -15, // sync and move to y, x
-	(signed char) -1, +14, -75, // draw, y, x
-	(signed char) -1, -36, +0, // draw, y, x
-	(signed char) +1, -4, -15, // sync and move to y, x
-	(signed char) -1, -14, -75, // draw, y, x
-	(signed char) -1, -72, +36, // draw, y, x
-	(signed char) -1, +0, +108, // draw, y, x
-	(signed char) -1, +72, +36, // draw, y, x
-	(signed char) +1, -10, -4, // sync and move to y, x
-	(signed char) -1, -104, -63, // draw, y, x
-	(signed char) +1, -10, +4, // sync and move to y, x
+{	(signed char) +1, +108, -67, // sync and move to y, x
+	(signed char) +0, +4, +0, // additional sync move to y, x
 	(signed char) -1, -102, +63, // draw, y, x
-	(signed char) +1, -4, +15, // sync and move to y, x
-	(signed char) -1, -14, +75, // draw, y, x
-	(signed char) -1, +36, +0, // draw, y, x
-	(signed char) +1, +4, +15, // sync and move to y, x
-	(signed char) -1, +14, +75, // draw, y, x
-	(signed char) -1, +72, -36, // draw, y, x
-	(signed char) -1, +0, -108, // draw, y, x
+	(signed char) -1, -6, -11, // draw, y, x
+	(signed char) -1, +14, -75, // draw, y, x
+	(signed char) -1, +72, +36, // draw, y, x
+	(signed char) -1, +0, +108, // draw, y, x
+	(signed char) -1, -72, +36, // draw, y, x
+	(signed char) -1, -36, +0, // draw, y, x
 	(signed char) -1, -72, -36, // draw, y, x
-	(signed char) +1, -63, +37, // sync and move to y, x
+	(signed char) -1, +0, -108, // draw, y, x
+	(signed char) -1, +72, -36, // draw, y, x
+	(signed char) -1, +14, +75, // draw, y, x
+	(signed char) -1, -6, +11, // draw, y, x
+	(signed char) -1, +0, +8, // draw, y, x
+	(signed char) -1, +6, +11, // draw, y, x
+	(signed char) -1, +8, +0, // draw, y, x
+	(signed char) +1, +4, +15, // sync and move to y, x
+	(signed char) -1, +6, -11, // draw, y, x
+	(signed char) -1, +0, -8, // draw, y, x
+	(signed char) +0, +0, +8, // mode, y, x
+	(signed char) -1, +99, +60, // draw, y, x
+	(signed char) +0, -73, -45, // mode, y, x
+	(signed char) -1, -27, +17, // draw, y, x
+	(signed char) -1, -18, +0, // draw, y, x
+	(signed char) -1, -27, -17, // draw, y, x
+	(signed char) -1, +0, -38, // draw, y, x
+	(signed char) -1, +27, -17, // draw, y, x
+	(signed char) -1, +18, +0, // draw, y, x
+	(signed char) -1, +27, +17, // draw, y, x
+	(signed char) -1, +0, +38, // draw, y, x
+	(signed char) +0, -32, -4, // mode, y, x
+	(signed char) -1, +14, +75, // draw, y, x
+	(signed char) +1, -108, -67, // sync and move to y, x
+	(signed char) +0, -6, +0, // additional sync move to y, x
+	(signed char) -1, +104, +63, // draw, y, x
+	(signed char) +0, +0, +8, // mode, y, x
+	(signed char) -1, -102, +63, // draw, y, x
+	(signed char) +0, +49, -30, // mode, y, x
 	(signed char) -1, +53, +26, // draw, y, x
 	(signed char) -1, +23, +0, // draw, y, x
 	(signed char) -1, +50, -27, // draw, y, x
@@ -48,15 +54,12 @@ const signed char web[]=
 	(signed char) -1, -26, +0, // draw, y, x
 	(signed char) -1, -50, +24, // draw, y, x
 	(signed char) -1, +0, +72, // draw, y, x
-	(signed char) +1, +36, +19, // sync and move to y, x
-	(signed char) -1, +0, -38, // draw, y, x
-	(signed char) -1, -27, -17, // draw, y, x
-	(signed char) -1, -18, +0, // draw, y, x
-	(signed char) -1, -27, +17, // draw, y, x
-	(signed char) -1, +0, +38, // draw, y, x
-	(signed char) -1, +27, +17, // draw, y, x
-	(signed char) -1, +18, +0, // draw, y, x
-	(signed char) -1, +27, -17, // draw, y, x
+	(signed char) +0, +45, +54, // mode, y, x
+	(signed char) -1, +14, -75, // draw, y, x
+	(signed char) +0, +0, -30, // mode, y, x
+	(signed char) -1, +8, +0, // draw, y, x
+	(signed char) +0, +14, -75, // mode, y, x
+	(signed char) -1, -36, +0, // draw, y, x
 	(signed char) +2 // endmarker 
 };
 
