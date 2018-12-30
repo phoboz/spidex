3734ae06b4a3f732836a3bcc4c2a04a921e6a62c
phoboz <phoboz@vmx.nu>
2018-12-30 14:43:27 +0300

    Checked web

--- source/web.c
+++ source/web.c
@@ -7,59 +7,59 @@
 // ---------------------------------------------------------------------------
 
 const signed char web[]=
-{	(signed char) +1, +108, -67, // sync and move to y, x
-	(signed char) +0, +4, +0, // additional sync move to y, x
-	(signed char) -1, -102, +63, // draw, y, x
-	(signed char) -1, -6, -11, // draw, y, x
-	(signed char) -1, +14, -75, // draw, y, x
-	(signed char) -1, +72, +36, // draw, y, x
-	(signed char) -1, +0, +108, // draw, y, x
-	(signed char) -1, -72, +36, // draw, y, x
-	(signed char) -1, -36, +0, // draw, y, x
-	(signed char) -1, -72, -36, // draw, y, x
-	(signed char) -1, +0, -108, // draw, y, x
-	(signed char) -1, +72, -36, // draw, y, x
-	(signed char) -1, +14, +75, // draw, y, x
-	(signed char) -1, -6, +11, // draw, y, x
-	(signed char) -1, +0, +8, // draw, y, x
-	(signed char) -1, +6, +11, // draw, y, x
-	(signed char) -1, +8, +0, // draw, y, x
-	(signed char) +1, +4, +15, // sync and move to y, x
-	(signed char) -1, +6, -11, // draw, y, x
-	(signed char) -1, +0, -8, // draw, y, x
-	(signed char) +0, +0, +8, // mode, y, x
-	(signed char) -1, +99, +60, // draw, y, x
-	(signed char) +0, -73, -45, // mode, y, x
-	(signed char) -1, -27, +17, // draw, y, x
-	(signed char) -1, -18, +0, // draw, y, x
-	(signed char) -1, -27, -17, // draw, y, x
-	(signed char) -1, +0, -38, // draw, y, x
-	(signed char) -1, +27, -17, // draw, y, x
-	(signed char) -1, +18, +0, // draw, y, x
-	(signed char) -1, +27, +17, // draw, y, x
-	(signed char) -1, +0, +38, // draw, y, x
-	(signed char) +0, -32, -4, // mode, y, x
-	(signed char) -1, +14, +75, // draw, y, x
-	(signed char) +1, -108, -67, // sync and move to y, x
-	(signed char) +0, -6, +0, // additional sync move to y, x
-	(signed char) -1, +104, +63, // draw, y, x
-	(signed char) +0, +0, +8, // mode, y, x
-	(signed char) -1, -102, +63, // draw, y, x
-	(signed char) +0, +49, -30, // mode, y, x
-	(signed char) -1, +53, +26, // draw, y, x
-	(signed char) -1, +23, +0, // draw, y, x
+{	(signed char) +1, +18, +90, // sync and move to y, x
+	(signed char) -1, -14, -75, // draw, y, x
+	(signed char) +0, -8, +0, // mode, y, x
+	(signed char) -1, -14, +75, // draw, y, x
+	(signed char) +0, -94, -23, // mode, y, x
+	(signed char) -1, +102, -63, // draw, y, x
+	(signed char) +0, +0, -8, // mode, y, x
+	(signed char) -1, -104, -63, // draw, y, x
+	(signed char) +0, +96, -23, // mode, y, x
+	(signed char) -1, +36, +0, // draw, y, x
+	(signed char) +0, -14, +75, // mode, y, x
+	(signed char) -1, -8, +0, // draw, y, x
+	(signed char) +0, +40, +35, // mode, y, x
+	(signed char) +1, +36, +20, // sync and move to y, x
+	(signed char) -1, +0, -40, // draw, y, x
+	(signed char) -1, -28, -16, // draw, y, x
+	(signed char) -1, -16, +0, // draw, y, x
+	(signed char) -1, -28, +16, // draw, y, x
+	(signed char) -1, +0, +40, // draw, y, x
+	(signed char) -1, +28, +16, // draw, y, x
+	(signed char) -1, +16, +1, // draw, y, x
+	(signed char) -1, +28, -17, // draw, y, x
+	(signed char) +0, +27, +16, // mode, y, x
+	(signed char) -1, +0, -73, // draw, y, x
+	(signed char) -1, -51, -23, // draw, y, x
+	(signed char) -1, -24, +0, // draw, y, x
+	(signed char) +1, -12, -60, // sync and move to y, x
+	(signed char) -1, -51, +24, // draw, y, x
+	(signed char) -1, +0, +73, // draw, y, x
+	(signed char) -1, +50, +26, // draw, y, x
+	(signed char) -1, +26, +0, // draw, y, x
 	(signed char) -1, +50, -27, // draw, y, x
-	(signed char) -1, +0, -72, // draw, y, x
-	(signed char) -1, -50, -24, // draw, y, x
-	(signed char) -1, -26, +0, // draw, y, x
-	(signed char) -1, -50, +24, // draw, y, x
-	(signed char) -1, +0, +72, // draw, y, x
-	(signed char) +0, +45, +54, // mode, y, x
-	(signed char) -1, +14, -75, // draw, y, x
-	(signed char) +0, +0, -30, // mode, y, x
-	(signed char) -1, +8, +0, // draw, y, x
-	(signed char) +0, +14, -75, // mode, y, x
-	(signed char) -1, -36, +0, // draw, y, x
+	(signed char) +0, +46, +28, // mode, y, x
+	(signed char) -1, -99, -60, // draw, y, x
+	(signed char) +0, +0, -8, // mode, y, x
+	(signed char) -1, +0, +8, // draw, y, x
+	(signed char) -1, -6, +11, // draw, y, x
+	(signed char) -1, -8, +0, // draw, y, x
+	(signed char) -1, -6, -11, // draw, y, x
+	(signed char) +1, -10, +4, // sync and move to y, x
+	(signed char) -1, +0, -8, // draw, y, x
+	(signed char) -1, +6, -11, // draw, y, x
+	(signed char) -1, -14, -75, // draw, y, x
+	(signed char) -1, -72, +38, // draw, y, x
+	(signed char) -1, +0, +105, // draw, y, x
+	(signed char) -1, +72, +37, // draw, y, x
+	(signed char) -1, +36, +0, // draw, y, x
+	(signed char) -1, +72, -37, // draw, y, x
+	(signed char) -1, +0, -106, // draw, y, x
+	(signed char) -1, -72, -37, // draw, y, x
+	(signed char) -1, -14, +75, // draw, y, x
+	(signed char) -1, +6, +11, // draw, y, x
+	(signed char) -1, +102, -63, // draw, y, x
 	(signed char) +2 // endmarker 
 };
 
