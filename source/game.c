83916ec7096eb6d846feb71dee5237493a104e7d
phoboz <phoboz@vmx.nu>
2019-01-24 00:29:53 +0100

    Workaround for game crash

--- source/game.c
+++ source/game.c
@@ -61,22 +61,56 @@ void init_game(void)
 	init_wave(&game_wave);
 }
 
-static void clear_foods_game(void)
+void clear_game(void)
 {
 	unsigned int i;
 
+	for (i = 0; i < GAME_MAX_BULLETS; i++)
+	{
+		game_bullets[i].obj.prev = 0;
+		game_bullets[i].obj.next = 0;
+	}
+	bullet_list = 0;
+	bullet_free_list = 0;
+
+	for (i = 0; i < GAME_MAX_ENEMIES; i++)
+	{
+		game_enemies[i].ch.obj.prev = 0;
+		game_enemies[i].ch.obj.next = 0;
+	}
+	enemy_list = 0;
+	enemy_free_list = 0;
+
+	for (i = 0; i < GAME_MAX_PROJECTILES; i++)
+	{
+		game_projectiles[i].obj.prev = 0;
+		game_projectiles[i].obj.next = 0;
+	}
+	projectile_list = 0;
+	projectile_free_list = 0;
+
 	for (i = 0; i < GAME_MAX_FOOD; i++)
 	{
-		deinit_food(&game_food[i]);
+		game_food[i].obj.prev = 0;
+		game_food[i].obj.next = 0;
 	}
+	food_list = 0;
+	food_free_list = 0;
+
+	for (i = 0; i < GAME_MAX_WALLS; i++)
+	{
+		game_walls[i].obj.prev = 0;
+		game_walls[i].obj.next = 0;
+	}
+	wall_list = 0;
+	wall_free_list = 0;
 }
 
 void restart_game(void)
 {
 	close_wave(&game_wave);
-	clear_foods_game();
-	init_player(&player_1, GAME_PLAYER_1_START_Y, GAME_PLAYER_1_START_X);
-	init_wave(&game_wave);
+	clear_game();
+	init_game();
 }
 
 void init_food_game(
